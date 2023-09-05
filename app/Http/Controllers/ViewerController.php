f485ab43085cd261886ccf49d02c8a5e33e8d463
Abdulkader-HM <Aboude-hm7@hotmail.com>
2023-09-05 09:30:12 +0200

    first commet after  creator, viewer,admin MVC

---
+++ app/Http/Controllers/ViewerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\viewer;
+use Illuminate\Http\Request;
+
+class ViewerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(viewer $viewer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(viewer $viewer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, viewer $viewer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(viewer $viewer)
+    {
+        //
+    }
+}
